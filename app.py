cf9d3ca674d272e5af69e2547d4410789ea61d61
Maya Sharma <mayasharma95@outlook.com>
2021-09-23 18:37:41 +0000

    Allow admin to update a genre name

--- app.py
+++ app.py
@@ -205,6 +205,20 @@ def add_genre():
     return render_template("add_genre.html")
 
 
+@app.route("/edit_genre/<genre_id>", methods=["GET", "POST"])
+def edit_genre(genre_id):
+    if request.method == "POST":
+        submit = {
+            "genre_name": request.form.get("genre_name")
+        }
+        mongo.db.genres.update({"_id": ObjectId(genre_id)}, submit)
+        flash("Genre Successfully Updated")
+        return redirect(url_for("get_genres"))
+
+    genre = mongo.db.genres.find_one({"_id": ObjectId(genre_id)})
+    return render_template("edit_genre.html", genre=genre)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
